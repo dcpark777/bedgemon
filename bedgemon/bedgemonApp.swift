cfc9cce75b7e08dc02ef1f8f5c902c5905ae6c66
Dan Park <dan.park.primary@gmail.com>
2026-02-11 10:46:36 -0500

    Initial Commit

---
+++ bedgemon/bedgemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  bedgemonApp.swift
+//  bedgemon
+//
+//  Created by Daniel Park on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct bedgemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
